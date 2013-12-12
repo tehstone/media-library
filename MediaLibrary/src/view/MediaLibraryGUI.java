6e978448f3fb12a0ebe5452c49aa192ba8d1bebf
Jonathan Stone <stone.johnny@gmail.com>
2013-12-12 15:35:13 -0800

    added tables to local DB and generated table column names from db query.

--- MediaLibrary/src/view/MediaLibraryGUI.java
+++ MediaLibrary/src/view/MediaLibraryGUI.java
@@ -2,19 +2,20 @@ package view;
 
 import actions.AboutAction;
 import actions.ExitAction;
-import actions.LabelAction;
 import actions.NewItemAction;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Action;
-import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -37,17 +38,26 @@ import javax.swing.table.DefaultTableModel;
  */
 public class MediaLibraryGUI {
 
+    /** DB Driver package info. */
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    /** URL for local DB. */
+    private static final String DB_URL = "jdbc:mysql://localhost/";
+    /** User name to connect to DB. */
+    private static final String USER = "eclipse";    
+    /** Password to connect to DB. */
+    private static final String PASS = "_ifbqGv+3RKg";
+
     /** Default Dimension size of the JFrame. */
     private static final Dimension DEFAULT_SIZE = new Dimension(1000, 800);
 
     /** String constants containing menu titles. */
     private static final String[] MENU_STRINGS = {"File", "Options", "Help"};
-
     /** String constants containing menu item names. */
     private static final String[] MENU_ITEM_STRINGS = {"New Item", "Exit", "About"};
-
     /** String constants containing tab names. */
     private static final String[] TAB_NAMES = {"CDs", "DVDs", "Books", "XBox Games"};
+    /** String constants containing tab names. */
+    private static final String[] TABLE_NAMES = {"cds", "dvds", "books", "xbox"};
 
     /** JFrame containing all elements comprising the GUI. */
     private JFrame myFrame;
@@ -59,11 +69,11 @@ public class MediaLibraryGUI {
         initializeFields();
 
         makeMenu();
-        
+
         makeCenter();
 
         makeSouth();
-        
+
         startGUI();
     }
 
@@ -73,6 +83,73 @@ public class MediaLibraryGUI {
     private void initializeFields() {
         myFrame = new JFrame();
         myFrame.setSize(DEFAULT_SIZE);
+    }
+
+    private String[][] connectToDB() {
+        Connection conn = null;
+        Statement stmt = null;
+        final String[][] columnNames = {{null, null, null}, 
+                        {null, null}, 
+                        {null, null}, 
+                        {null}};
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            stmt = conn.createStatement();
+
+//            final String query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'media' AND TABLE_NAME = 'cds';";
+
+
+            for (int i = 0; i < TABLE_NAMES.length; i++) {
+                ResultSet rs = stmt.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'media' AND TABLE_NAME = '" + TABLE_NAMES[i] + "';");
+                int j = 0;
+                try {
+                    while (rs.next()) {
+                        String s = rs.getString("COLUMN_NAME");
+                        if (!"id".equals(s)) {
+                            columnNames[i][j++] = rs.getString("COLUMN_NAME");
+                        }
+                    }
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+
+
+            //            while (rs.next()) {
+            //                System.out.println(rs.getString("COLUMN_NAME"));
+            //            }
+        } catch (final SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (final Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (final SQLException se2) {
+            } // nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (final SQLException se) {
+                se.printStackTrace();
+            } //end finally try
+        } //end try
+        return columnNames;
     }
     /**
      * Generates and populates Menus and adds them to MenuBar.
@@ -115,49 +192,51 @@ public class MediaLibraryGUI {
         // JTable
         final JTabbedPane tabPane = new JTabbedPane();
 
-        final String[][] columnNames = {{"Title", "Artist", "Genre"}, 
-                                        {"Title", "Year"}, 
-                                        {"Title", "Author"}, 
-                                        {"Title"}};
-        
+        final String[][] columnNames = connectToDB();
+
+        //        = {{"Title", "Artist", "Genre"}, 
+        //                        {"Title", "Year"}, 
+        //                        {"Title", "Author"}, 
+        //                        {"Title"}};
+
         final Object[][][] data = {
-            {
-                {"Blue", "Third Eye Blind", "Pop-Punk"}, {"Ten", "Pearl Jam", "Grunge"}
-            }, {
-                {"Pacific Rim", "2013"}
-            }, {
-                {"Core Java", "Horstmann"}
-            }, {
-                {"DDR Revolution"}
-            }
+                        {
+                            {"Blue", "Third Eye Blind", "Pop-Punk"}, {"Ten", "Pearl Jam", "Grunge"}
+                        }, {
+                            {"Pacific Rim", "2013"}
+                        }, {
+                            {"Core Java", "Horstmann"}
+                        }, {
+                            {"DDR Revolution"}
+                        }
         };
 
         final List<JTable> tables = new ArrayList<JTable>();
         for (int i = 0; i < columnNames.length; i++) {
             @SuppressWarnings("serial")
             final DefaultTableModel tableModel = 
-                            new DefaultTableModel(data[i], columnNames[i]) {
-                    @Override
-                    public boolean isCellEditable(final int theRow, final int theColumn) {
+            new DefaultTableModel(data[i], columnNames[i]) {
+                @Override
+                public boolean isCellEditable(final int theRow, final int theColumn) {
                     //all cells false
-                        return false;
-                    }
-                };
+                    return false;
+                }
+            };
             final JTable table = new JTable(tableModel);
             table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-                    @Override
-                    public void valueChanged(final ListSelectionEvent theEvent) {
-                        // do some actions here, for example
-                        // print first column value from selected row
-                        final StringBuilder string = new StringBuilder();
-                        for (int i = 0; i < table.getColumnCount(); i++) {
-                            string.append(table.getValueAt(table.getSelectedRow(), i).
-                                          toString().
-                                          replaceAll("\\s+", ""));
-                        }
-                        System.out.println(string.toString());
+                @Override
+                public void valueChanged(final ListSelectionEvent theEvent) {
+                    // do some actions here, for example
+                    // print first column value from selected row
+                    final StringBuilder string = new StringBuilder();
+                    for (int i = 0; i < table.getColumnCount(); i++) {
+                        string.append(table.getValueAt(table.getSelectedRow(), i).
+                                      toString().
+                                      replaceAll("\\s+", ""));
                     }
-                });
+                    System.out.println(string.toString());
+                }
+            });
             tables.add(table);
         }
 
@@ -178,18 +257,18 @@ public class MediaLibraryGUI {
         final JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         // JPanel (grid layout?) border.south
-        
+
         final JLabel imageLabel = new JLabel();
         imageLabel.addPropertyChangeListener("selected", null);
         final int size = myFrame.getWidth() / 3; 
         imageLabel.setPreferredSize(new Dimension(size, size));
         Icon icon = new ImageIcon();
         imageLabel.setIcon(icon);
-        
+
         // JLabel (left 1/3rd of panel)
         final JTextArea infoText = new JTextArea();
         infoText.setPreferredSize(new Dimension(size * 2, size));
-        
+
         // JTextArea (remainder of panel)
         panel.add(imageLabel, BorderLayout.WEST);
         panel.add(infoText, BorderLayout.EAST);
@@ -204,7 +283,7 @@ public class MediaLibraryGUI {
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setLocationByPlatform(true);
         myFrame.pack();
-        
+
         myFrame.setVisible(true);
     }
 }
--- MediaLibrary/src/view/MediaLibraryMain.java
+++ MediaLibrary/src/view/MediaLibraryMain.java
@@ -3,24 +3,12 @@
  */
 package view;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 /**
  * @author jrsto674
  * @version 12/11/2013
  */
 public final class MediaLibraryMain {
-
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-    static final String DB_URL = "jdbc:mysql://localhost/";
-
-    //  Database credentials
-    static final String USER = "eclipse";
-    static final String PASS = "_ifbqGv+3RKg";
-
+    
     /**
      * 
      */
@@ -32,50 +20,7 @@ public final class MediaLibraryMain {
      * @param theArgs Possible command line arguments.
      */
     public static void main(final String... theArgs) {        
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            //STEP 2: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-            //STEP 4: Execute a query
-            System.out.println("Creating database...");
-            stmt = conn.createStatement();
-
-            final String sql = "DROP DATABASE STUDENTS";
-            stmt.executeUpdate(sql);
-            System.out.println("Database created successfully...");
-        } catch (final SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (final Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-            } catch (final SQLException se2) {
-            } // nothing we can do
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (final SQLException se) {
-                se.printStackTrace();
-            } //end finally try
-        } //end try
-        System.out.println("Goodbye!");
-        //new MediaLibraryGUI();
-        //STEP 1. Import required packages
-
-
+        new MediaLibraryGUI();
     }
 
 }
